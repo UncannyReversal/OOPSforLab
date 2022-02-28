c2c501f4fffdb8398fcdfeae6478de74616cca1e
UncannyReversal <100613725+UncannyReversal@users.noreply.github.com>
2022-02-28 23:46:30 +0530

    Add files via upload

---
+++ Odd_even.java
@@ -0,0 +1,10 @@
+package odd_even;
+public class Odd_even {
+    public static void main(String[] args) {
+        int a = 15;
+        if(a % 2 == 0)
+            System.out.println("It is even number");
+        else
+            System.out.println("It is odd number");
+    }
+}
